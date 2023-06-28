3dc5fd4376b4bb74d58078afec52ec27a13befa2
Miklós Gyula <ii.miklosgyula@gmail.com>
2023-06-28 16:29:37 +0200

    Adding turns, herritage, health, game reset, ranks

--- Clan_Simulator/Clan_Simulator_Browser/models.py
+++ Clan_Simulator/Clan_Simulator_Browser/models.py
@@ -4,9 +4,11 @@ class Member(models.Model):
     name = models.CharField(max_length=25)
     vitality = models.IntegerField()
     resilience = models.IntegerField()
+    health = models.IntegerField(default=100)
+    age = models.IntegerField(default=0)
     pcn = models.IntegerField()
     childnumber = models.IntegerField()
-    position = models.TextField(max_length=20)
+    position = models.TextField(max_length=20) #pawn, citizen, baron, count, king
     balance = models.IntegerField()
     alive = models.BooleanField(default=True)
     fatherid = models.IntegerField()
